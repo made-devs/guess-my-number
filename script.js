352d0a370e884c18cfcb35499146582e41cd7ede
nmaderama <nmaderama@gmail.com>
2022-07-06 16:28:04 +0700

    Second Commit - Handling Click Event

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 'use strict';
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '😣 Input your number!';
+  }
+});
