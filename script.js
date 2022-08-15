b92dbfc9e885cd3eee5483a59d9af95ea1638f09
madedev <nmaderama@gmail.com>
2022-08-15 12:56:36 +0700

    Second commit - Implementing logic

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
 'use strict';
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 5;
+document.querySelector('.score').textContent = score;
+document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
   if (!guess) {
     document.querySelector('.message').textContent = '😣 Input your number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎊 Your number correct!';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent =
+        '📈 Your number too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent =
+        '📉 Your number too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
+
+console.log(secretNumber);
